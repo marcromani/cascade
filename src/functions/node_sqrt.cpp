07fbc9ea78054bd58319e53cfdb1e9582ea84420
Marc Romaní <marcromani.ub@gmail.com>
2024-03-30 19:14:29 +0100

    Fix derivative

--- src/functions/node_sqrt.cpp
+++ src/functions/node_sqrt.cpp
@@ -6,6 +6,6 @@ void NodeSqrt::backprop_()
 {
     std::shared_ptr<Node> x = children_.at(0);
 
-    x->setDerivative(x->derivative() - (1.0 / (x->value() * x->value())) * derivative_);
+    x->setDerivative(x->derivative() + (0.5 / value_) * derivative_);
 }
 }  // namespace cascade
