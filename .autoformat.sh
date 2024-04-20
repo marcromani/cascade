e6299e40ddf21c1f4c61885609f7364b09ffed91
Marc Romaní <marcromani.ub@gmail.com>
2024-04-20 12:29:35 +0200

    Refactor
    Separate code in `var`and `tensor`folders
    Improve compilation speed by creating object files and reusing them to build each library

--- .autoformat.sh
+++ .autoformat.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-clang-format -i src/*.cpp src/*.h src/*.h.inl src/functions/*.cpp src/functions/*.h src/kernels/*.cu src/kernels/*.h
+clang-format -i src/*.h
+clang-format -i src/var/*.cpp src/var/*.h src/var/functions/*.cpp src/var/functions/*.h
+clang-format -i src/tensor/*.cpp src/tensor/*.h src/tensor/*.h.inl src/tensor/kernels/*.cu src/tensor/kernels/*.h
 clang-format -i tests/*.cpp tests/include/*.h
 clang-format -i examples/*.cpp
 
 cmake-format -c=.cmake-format CMakeLists.txt -o CMakeLists.txt
 cmake-format -c=.cmake-format src/CMakeLists.txt -o src/CMakeLists.txt
+cmake-format -c=.cmake-format src/var/CMakeLists.txt -o src/var/CMakeLists.txt
+cmake-format -c=.cmake-format src/tensor/CMakeLists.txt -o src/tensor/CMakeLists.txt
 cmake-format -c=.cmake-format tests/CMakeLists.txt -o tests/CMakeLists.txt
 cmake-format -c=.cmake-format examples/CMakeLists.txt -o examples/CMakeLists.txt
